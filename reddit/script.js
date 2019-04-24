7561591862045ec0ca52fe5527cce45bb3c2355f
Glenn Henshaw <glenn@Glenns-MacBook-Pro.local>
2019-04-24 18:07:26 -0400

    slider default

--- reddit/script.js
+++ reddit/script.js
@@ -142,7 +142,7 @@ d3.csv("data.csv").then(function(data){
 
     d3.interval(function(){
  	    update(data,xscale,yscale,colorscale);
-    },500);
+    },1000);
     update(data,xscale,yscale,colorscale);
 
       
