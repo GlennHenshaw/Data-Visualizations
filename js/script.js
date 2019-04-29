197720e343262d5e230d8f78c4bcb4e9be75f7c2
Glenn Henshaw <glenn@unknown.local>
2019-04-29 04:24:28 -0400

    new index page

--- js/script.js
+++ js/script.js
@@ -3,15 +3,15 @@
 //////////////////////////////////////////////
 // fake data
 var data = [
-    {year: 2000, population: 1192},
-    {year: 2001, population: 1234},
-    {year: 2002, population: 1463},
+    {year: 2000, population: 300},
+    {year: 2001, population: 732},
+    {year: 2002, population: 520},
     {year: 2003, population: 1537},
     {year: 2004, population: 1334},
     {year: 2005, population: 1134},
-    {year: 2006, population: 1234},
-    {year: 2007, population: 1484},
-    {year: 2008, population: 1562},
+    {year: 2006, population: 800},
+    {year: 2007, population: 750},
+    {year: 2008, population: 1000},
     {year: 2009, population: 1427},
     {year: 2010, population: 1325},
     {year: 2011, population: 1484},
@@ -22,13 +22,9 @@ var data = [
     {year: 2016, population: 1427}
 ];
 
-var num = 1;
 
-
-// Parse the date / time
 var parseDate = d3.timeParse("%Y");
 
-// force types
 function type(dataArray) {
     dataArray.forEach(function(d) {
         d.year = parseDate(d.year);
@@ -36,6 +32,16 @@ function type(dataArray) {
     });
     return dataArray;
 }
+
+
+var num = 1;
+
+
+// Parse the date / time
+
+
+// force types
+
 data = type(data);
 
 //////////////////////////////////////////////
@@ -56,44 +62,57 @@ var xAxis = d3.axisBottom().scale(xScale);
 var yAxis = d3.axisLeft().scale(yScale);
 
 // create a line
-var line = d3.line();
+var line = d3.line()
+             .curve(d3.curveNatural);
 
 // Add the svg canvas
 var svg = d3.select("#chart-area")
     .append("svg")
         .attr("height", height + margin.top + margin.bottom);
 
-var artboard = svg.append("g")
+var g = svg.append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 // set the domain range from the data
 xScale.domain(d3.extent(data, function(d) { return d.year; }));
 yScale.domain([
-        d3.min(data, function(d) { return Math.floor(d.population - 200); }), 
+        d3.min(data, function(d) { return 0; }), 
         d3.max(data, function(d) { return Math.floor(d.population + 200); })
     ]);
 
 // draw the line created above
-var path = artboard.append("path")
+
+
+ var area_path = g.append("path")
+                  .attr("class", "area");
+
+ var path = g.append("path")
         .style('fill', 'none')
+        .attr("class","curve")
         .style('stroke', '#E3BAB3')
-        .style('stroke-width', '5px');
+        .style('stroke-width', '2px');
+
+
 
 // Add the X Axis
-var xAxisEl = artboard.append("g")
+var xAxisEl = g.append("g")
         .attr("transform", "translate(0," + height + ")");
 
 // Add the Y Axis
 // we aren't resizing height in this demo so the yAxis stays static, we don't need to call this every resize
-var yAxisEl = artboard.append("g")
-        .call(yAxis)
+var yAxisEl = g.append("g")
+        .call(yAxis.ticks(5))
         .attr("stroke","#E9DCCD");
 
 //////////////////////////////////////////////
 // Drawing ///////////////////////////////////
 //////////////////////////////////////////////
 function drawChart() {
-    // reset the width
+
+    d3.selectAll(".xgrid").remove();
+    d3.selectAll(".ygrid").remove();
+    //d3.selectAll("path.domain").remove();
+
     width = parseInt(d3.select('.jumbotron').style('width'), 10) - margin.left - margin.right;
     // set the svg dimensions
     svg.attr("width", width + margin.left + margin.right);
@@ -106,28 +125,65 @@ function drawChart() {
     
     // draw the new xAxis
     xAxisEl.call(xAxis);
+
+
+
+    path.data([data]);
+
+
+    var area = d3.area()
+                 .x(function(d,i) {
+                     return xScale(d.year);
+                 })
+                 .y0(height)
+                 .y1(function(d) {return yScale(d.population); })
+                 .curve(d3.curveNatural)
+                 .defined(function(d,i){
+                 if ((i>5)&&(i<14)){
+                     return true;
+                 } else {
+                     return false;
+                 }
+    });
+
     
-    // specify new properties for the line
 
-    d3.interval(function(){
-        data_new = data.slice(0,num);
-        console.log(data_new);
+    
 
-        path.data([data_new]);
 
-        line.x(function(d) { 
+    var xgridlines = d3.axisBottom()
+                    .tickFormat("")
+                    .tickSize(height)
+                    .scale(xScale);
+    var ygridlines = d3.axisLeft()
+                    .tickFormat("")
+                    .tickSize(-width)
+                    .scale(yScale);
+
+     g.append("g")
+     .attr("class", "xgrid")
+     //.attr("transform", "translate(0," + height + ")")
+     .style("stroke-dasharray", "4,4")
+     .call(xgridlines);
+
+     g.append("g")
+     .attr("class", "ygrid")
+     .style("stroke-dasharray", "4,4")
+     .call(ygridlines.ticks(5));
+
+
+   area_path.data([data])
+                .attr("d", area)
+                .attr("class","region");
+
+    line.x(function(d) { 
             return xScale(d.year); })
         .y(function(d) { 
             return yScale(d.population); });
-        num = num+1;
 
-        if (num == 17){
-            num = 1;
-            path.exit().remove();
-        }
+    path.attr('d', line);
 
-        path.attr('d', line);
-     },200);
+
 };
     
     
